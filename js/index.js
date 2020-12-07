1e17ea2f6870b60097fa2de1db90bd2b1abad26d
Emma Jaud <jaud2@att.net>
2020-12-07 08:39:23 -0600

    img hover

--- js/index.js
+++ js/index.js
@@ -9,6 +9,49 @@ $("#logo").click(function() {
 
 
 // HOVER STATES
+$("button").hover(function() {
+    var shadow = $(this).css("box-shadow");
+    if (shadow == "rgba(0, 0, 0, 0.725) 0px 3px 6px 0px"){
+        $(this).css("box-shadow", "0px 3px 6px #00000029");
+    }
+    else {
+        $(this).css("box-shadow", "0px 3px 6px #000000B9");
+    }
+});
+
+var scaleW = "no";
+$("#img-1").hover(function() {
+    var w = $("#img-1").width();
+    var newW = w * 1.02;
+
+    if (scaleW == "yes"){
+        var changeBack = w / 1.02;
+        $(this).animate({width: changeBack});
+        scaleW = "no";
+    }
+    else {
+        $(this).animate({width: newW});
+        scaleW = "yes";
+        console.log(scaleW)
+    }
+});
+$("#img-2").hover(function() {
+    var w2 = $(this).width();
+    var newW2 = w2 * 1.02;
+
+    if (scaleW == "yes"){
+        var changeBack = w2 / 1.02;
+        $(this).animate({width: changeBack});
+        scaleW = "no";
+    }
+    else {
+        $(this).animate({width: newW2});
+        scaleW = "yes";
+        console.log(scaleW)
+    }
+});
+
+
 $(".workHover").hover(function() {
     var decor = $(this).css("text-decoration");
     if (decor == "underline solid rgb(0, 0, 0)"){
