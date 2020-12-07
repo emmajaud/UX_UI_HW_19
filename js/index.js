30883df51d20cbc4b34abced86d34bd4bd82d014
Emma Jaud <jaud2@att.net>
2020-12-07 07:35:40 -0600

    hover states

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
 // UI Interactions
+console.log("this works")
+
+
+// HOVER STATES
+$(".workHover").hover(function() {
+    var decor = $(this).css("text-decoration");
+    if (decor == "underline solid rgb(0, 0, 0)"){
+        $(".workHover").css("text-decoration", "none");
+    }
+    else {
+        $(".workHover").css("text-decoration", "underline");
+    }
+    console.log(decor);
+});
+$(".aboutHover").hover(function() {
+    var decor = $(this).css("text-decoration");
+    if (decor == "underline solid rgb(0, 0, 0)"){
+        $(".aboutHover").css("text-decoration", "none");
+    }
+    else {
+        $(".aboutHover").css("text-decoration", "underline");
+    }
+    console.log(decor);
+});
+$(".contactHover").hover(function() {
+    var decor = $(this).css("text-decoration");
+    if (decor == "underline solid rgb(0, 0, 0)"){
+        $(".contactHover").css("text-decoration", "none");
+    }
+    else {
+        $(".contactHover").css("text-decoration", "underline");
+    }
+    console.log(decor);
+});
+$(".resumeHover").hover(function() {
+    var decor = $(this).css("text-decoration");
+    if (decor == "underline solid rgb(0, 0, 0)"){
+        $(".resumeHover").css("text-decoration", "none");
+    }
+    else {
+        $(".resumeHover").css("text-decoration", "underline");
+    }
+    console.log(decor);
+});
+$(".linkedinHover").hover(function() {
+    var decor = $(this).css("text-decoration");
+    if (decor == "underline solid rgb(0, 0, 0)"){
+        $(".linkedinHover").css("text-decoration", "none");
+    }
+    else {
+        $(".linkedinHover").css("text-decoration", "underline");
+    }
+    console.log(decor);
+});
